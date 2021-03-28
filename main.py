d15d7905a3bae9c9a77fc080b5564ce973d2a01e
Pitkov Denis <den70007.ua@gmail.com>
2021-03-28 17:13:43 +0300

    update main file

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import re
 
-regex = r'(.*?) - - \[(\d{2}\/\D{3}\/\d{4}):(\d{2}:\d{2}:\d{2}) -\d+\] "(.*?)" (\d+) (\d+)'
+regex = r'.*? - - \[(\d{2}\/\D{3}\/\d{4}):(\d{2}:\d{2}:\d{2}) -\d+\] "GET .*?" 200 (.+)'
 
 
-def search_data(data_string: str):
-    data = re.match(regex, data_string)
-    if data:
-        if 'GET' in data[4] and data[5] == '200':
-            time = sum(int(x) * 60 ** i for i, x in enumerate(reversed(data[3].split(':'))))
-            start_time, end_time = 35 * 60, 1 * 60 ** 2 + 12 * 60
-            return int(data[6]) if data[2] == '01/Jul/1995' and start_time <= time <= end_time else 0
-    return None
+def convert_time(time): return sum(int(x) * 60 ** i for i, x in enumerate(reversed(time.split(':'))))
+
+
+def get_bytes(string_data):
+    return int(string_data[3]) if string_data[1] == '01/Jul/1995' and \
+                                  convert_time("00:35:00") <= convert_time(data[2]) <= convert_time("1:12:00") and string_data[3] != '-' else 0
 
 
 if __name__ == '__main__':
     with open('access_log_Jul95', 'r') as file:
-        all_bytes = [search_data(i) for i in file.readlines() if search_data(i)]
-        print(f"Кількість успішних GET запитів {len(all_bytes)}, сумарна кількість байт {sum(all_bytes)}")
+        successful, bytes_size = 0, 0
+        for line in file.readlines():
+            data = re.match(regex, line)
+            if data: bytes_size, successful = bytes_size + get_bytes(data), successful + 1
+        print(f"Кількість успішних GET запитів {successful}, сумарна кількість байт {bytes_size}")
